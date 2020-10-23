ee572e85fea7d6916a244d4f5794a38c8c248427
sat0ken <15720506+sat0ken@users.noreply.github.com>
2020-10-24 00:24:55 +0900

    add engine runtime

--- exporter.py
+++ exporter.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from prometheus_client import start_http_server, Gauge
+from prometheus_client import start_http_server, Gauge, Counter
 import random
 import time
 import obd
@@ -12,13 +12,15 @@ COOLANT_TEMP        = Gauge('coolant_temp', 'Engine coolant temperature')
 SHORT_FUEL_TRIM_1   = Gauge('short_fuel_trim_1', 'Short term fuel trim Bank 1')
 RPM                 = Gauge('rpm', 'Engine speed')
 SPEED               = Gauge('speed', 'Vehicle speed')
+RUN_TIME            = Counter('run_time', 'Run time since engine start')
 
 commands = {
     obd.commands.ENGINE_LOAD: ENGINE_LOAD,
     obd.commands.COOLANT_TEMP: COOLANT_TEMP,
     obd.commands.SHORT_FUEL_TRIM_1: SHORT_FUEL_TRIM_1 ,
     obd.commands.RPM: RPM,
-    obd.commands.SPEED: SPEED
+    obd.commands.SPEED: SPEED,
+    obd.commands.RUN_TIME: RUN_TIME
 }
 
 def get_obd(connection):
